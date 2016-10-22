faabfe3408bc2befb0ee712cee59e4a24b781c8f
Andrew Stout <andrew@andrewstout.me>
2016-10-21 23:09:08 -0400

    Setup program to check if secret is additive

--- Projects/AdditiveSecretFunction/Program.cs
+++ Projects/AdditiveSecretFunction/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace AdditiveSecretFunction
+﻿using System;
+
+namespace AdditiveSecretFunction
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int limit;
+            if (!int.TryParse(args[0], out limit))
+            {
+                Console.WriteLine("Invalid input");
+                Console.Read();
+                Environment.Exit(0);
+            }
+
+            var primes = PrimeNumberHelper.GetPrimesLessThan(limit);
+
+            foreach (var x in primes)
+            {
+                foreach (var y in primes)
+                {
+                    if (SecretService.Secret(x + y) != (SecretService.Secret(x) + SecretService.Secret(y)))
+                    {
+                        Console.WriteLine("Not Additive");
+                        Console.Read();
+                        Environment.Exit(0);
+                    }
+                }
+            }
+
+            Console.WriteLine("Additive");
+            Console.Read();
         }
     }
 }
