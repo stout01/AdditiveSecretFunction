08da61e4b66182fd2729c2d9b46d2252c11fca0c
Andrew Stout <andrew@andrewstout.me>
2016-10-23 23:13:02 -0400

    Setup tests for AdditiveService

---
+++ Tests/AdditiveSecretFunction.Services.Tests/AdditiveServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using AdditiveSecretFunction.Services.Abstract;
+using AdditiveSecretFunction.Services.Concrete;
+using Xunit;
+
+namespace AdditiveSecretFunction.Services.Tests
+{
+    public class AdditiveServiceTests
+    {
+        private readonly IAdditiveService _additiveService;
+
+        public AdditiveServiceTests()
+        {
+            _additiveService = new AdditiveService(new PrimeNumberService());
+        }
+
+        [Fact]
+        public void IsAdditive()
+        {
+            Assert.True(_additiveService.IsAdditive(x => x, 100));
+        }
+
+        [Fact]
+        public void IsNotAdditive()
+        {
+            Assert.False(_additiveService.IsAdditive(x => x / 5, 100));
+        }
+    }
+}
